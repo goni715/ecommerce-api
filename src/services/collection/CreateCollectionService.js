bdd0a4bb779cf64af0db1d9b608f82b5ea60470e
goni715 <halesgoni715149123@gmail.com>
2024-06-19 01:13:17 +0600

    duplicate-collection-handle

--- src/services/collection/CreateCollectionService.js
+++ src/services/collection/CreateCollectionService.js
@@ -3,32 +3,39 @@ const cloudinary = require("../../helper/cloudinary");
 
 const CreateCollectionService = async (req, res, CollectionModel) => {
     try{
+        const {title} = req.body;
+        const slug = slugify(title);
+        let existingCollection =await CollectionModel.findOne({slug:slug});
 
-        let cloudinaryResponse;
-        if(req.file) {
-            cloudinaryResponse = await cloudinary.uploader.upload(req.file.path, {
-                folder: "Borcella-ecommerce/collections",
-            });
-
-        }else{
-            res.status(400).json('Please provide a file');
+        if(existingCollection){
+            res.status(409).json({message: "fail", data:"This collection name is Already taken"});
         }
+        else{
+            let cloudinaryResponse;
+            if(req.file) {
+                cloudinaryResponse = await cloudinary.uploader.upload(req.file.path, {
+                    folder: "Borcella-ecommerce/collections",
+                });
 
-        if(cloudinaryResponse){
-
-            const PostBody = req.body;
-            if(PostBody.title){
-                PostBody.slug=slugify(PostBody.title);
+            }else{
+                res.status(400).json('Please provide a file');
             }
 
-            PostBody.image={
-                public_id:cloudinaryResponse?.public_id,
-                image_url:cloudinaryResponse?.secure_url
+            if(cloudinaryResponse){
+
+                if(title){
+                    req.body.slug=slug
+                }
+
+                req.body.image={
+                    public_id:cloudinaryResponse?.public_id,
+                    image_url:cloudinaryResponse?.secure_url
+                }
+
+                let data = await CollectionModel.create(req.body)
+                res.status(201).json({message:"success",data:data, result: cloudinaryResponse});
+
             }
-
-            let data = await CollectionModel.create(PostBody)
-            res.status(201).json({message:"success",data:data, result: cloudinaryResponse});
-
         }
     }
     catch(error){
